e2fbadf7fb49f52297806d6b452ed88ea9ae820f
banjiefeng <2558000837@qq.com>
2026-04-08 15:36:10 +0800

    添加用户管理

---
+++ build/CMakeFiles/xdoj.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jinzheyu/graduate_design/src/AnnouncementList.cpp" "CMakeFiles/xdoj.dir/src/AnnouncementList.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/AnnouncementList.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/CommentList.cpp" "CMakeFiles/xdoj.dir/src/CommentList.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/CommentList.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/Control.cpp" "CMakeFiles/xdoj.dir/src/Control.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/Control.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/DiscussList.cpp" "CMakeFiles/xdoj.dir/src/DiscussList.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/DiscussList.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/HttpServer.cpp" "CMakeFiles/xdoj.dir/src/HttpServer.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/HttpServer.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/Judger.cpp" "CMakeFiles/xdoj.dir/src/Judger.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/Judger.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/MongoDataBase.cpp" "CMakeFiles/xdoj.dir/src/MongoDataBase.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/MongoDataBase.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/ProblemList.cpp" "CMakeFiles/xdoj.dir/src/ProblemList.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/ProblemList.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/RedisDataBase.cpp" "CMakeFiles/xdoj.dir/src/RedisDataBase.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/RedisDataBase.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/SolutionList.cpp" "CMakeFiles/xdoj.dir/src/SolutionList.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/SolutionList.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/StatusRecordList.cpp" "CMakeFiles/xdoj.dir/src/StatusRecordList.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/StatusRecordList.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/Tag.cpp" "CMakeFiles/xdoj.dir/src/Tag.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/Tag.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/UserList.cpp" "CMakeFiles/xdoj.dir/src/UserList.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/UserList.cpp.o.d"
+  "/home/jinzheyu/graduate_design/src/main.cpp" "CMakeFiles/xdoj.dir/src/main.cpp.o" "gcc" "CMakeFiles/xdoj.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
